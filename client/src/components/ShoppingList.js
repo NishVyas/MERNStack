3e786f6f8e707c0285e7dbb87b67641b527b1bb1
Nishant Vyas <nvyas3095@gmail.com>
2018-11-01 16:13:03 -0700

    Shopping List items are displayed. We now have the ability to add items and them being displayed as well. Created deletion icons for our items. Ability to delete items from our list using the filtering method , which does not communicate with our server side yet. Added some css for fading effects when we add and delete items.

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -36,7 +36,40 @@ class ShoppingList extends Component {
                  }}
                  >
                     Add Item
-                 </Button>    
+                </Button>
+                 
+                {/* Below we list all the items */}
+                <ListGroup>
+                    {/* We wrap the items around a Transition Group and traverse through each item using items.map */}
+                    {/* For each item, we want to display a listGroup item and add in a fading transition effect */}
+                    <TransitionGroup className="shopping-list">
+                        {/* The below traverses through each item and adds in the effect*/}
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                {/* Below, we wrap each item name inside the ListGroupItem tag */}
+                                <ListGroupItem>
+                                    {/* Below, we add in a delete button */}
+                                    {/* On click, we set an arrow function to the current state (which is items) */}
+                                    {/* Now we take the items in the state (state.items) and use the filter method */}
+                                    {/* We use 'item' as the variable in the filter method. If item.id is NOT EQUAL to the id of the item we clicked on, it does NOT show up in our array */}
+                                    {/* We basically filtered out the item that we clicked on using the id attribute. This is just another way of saying we delted out item */}
+                                    {/* The '&times;' just gives us the 'x' icon for deleting */}
+                                    <Button
+                                        className="remove-btn"
+                                        color="danger"
+                                        size="sm"
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+                                            }));
+                                        }}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>        
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>    
             </Container>
         );
     }
